5903aafc25c56aefb6c6e2f28ce110b19c372009
Matt Wittmann <mcwitt@gmail.com>
2018-06-11 21:33:39 -0700

    Migrate from antigen to grml-zsh-config

--- install.sh
+++ install.sh
@@ -5,3 +5,16 @@ for f in $files; do
     bn=$(basename $f)
     ln -s $(pwd)/$bn ~/.$bn
 done
+
+# zsh {{
+# grml zsh config
+wget -O ~/.zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+
+# install zsh scripts
+script_dir=~/.local/share
+mkdir -p $script_dir
+cd $script_dir
+
+# history substring search
+git clone git@github.com:zsh-users/zsh-history-substring-search.git
+# }}
