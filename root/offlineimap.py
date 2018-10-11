bef6b82dddfd6758565783b65925bd2081cbd2c5
Matt Wittmann <mcwitt@gmail.com>
2018-10-11 09:39:23 -0700

    Store mail passwords in authinfo

--- root/offlineimap.py
+++ root/offlineimap.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
+import re
 from subprocess import check_output
 
 
-def get_password(account):
-    key = 'Mail/' + account
-    return check_output(['pass', key]).decode('utf-8').strip()
+def get_password_emacs(machine, login, port):
+    s = 'machine %s login %s port %s password ([^\n ]*)' % (machine, login, port)
+    p = re.compile(s)
+    b = check_output('gpg -q --no-tty -d ~/.authinfo.gpg', shell=True)
+    authinfo = b.decode()
+    return p.search(authinfo).group(1)
