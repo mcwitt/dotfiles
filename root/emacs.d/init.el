d819e91e60eef511c7efeaf471f34a292203c945
Matt Wittmann <matt@takescoop.com>
2019-01-21 11:11:08 -0800

    Remove customize code, set custom-file

--- root/emacs.d/init.el
+++ root/emacs.d/init.el
@@ -167,17 +167,6 @@
 (defun haskell-mode-format-buffer-with-brittany () (interactive)
   (shell-command-to-string (format "brittany --write-mode inplace %s" buffer-file-name))
   (revert-buffer :ignore-auto :noconfirm))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (intero persp-mode ensime evil-magit magit company counsel-projectile projectile general which-key counsel ivy zenburn-theme evil-escape evil use-package))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
+(setq custom-file "~/.emacs-custom.el")
+
