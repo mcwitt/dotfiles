07643252651e7290d733c88d9e32e716c7d5eb1b
Matt Wittmann <matt@takescoop.com>
2019-01-27 22:28:09 -0800

    Keybindings for org capture and agenda

--- root/emacs.d/init.el
+++ root/emacs.d/init.el
@@ -67,7 +67,8 @@
 
 (global-set-key (kbd "C-c g") 'counsel-git)
 (global-set-key (kbd "C-c j") 'counsel-git-grep)
-(global-set-key (kbd "C-c k") 'counsel-ag)
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
 (global-set-key (kbd "C-x l") 'counsel-locate)
 
 (global-set-key (kbd "C-c C-r") 'ivy-resume)
