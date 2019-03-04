75d5f5594efe99a973702cfad8feea7cabeed93a
Matt Wittmann <matt@takescoop.com>
2019-03-04 15:44:32 -0800

    Configure markdown-mode

--- root/emacs.d/init.el
+++ root/emacs.d/init.el
@@ -184,7 +184,13 @@
      (R . t))))
 
 ;; Markdown
-(use-package markdown-mode)
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc"))
 
 ;; Haskell
 (use-package intero
