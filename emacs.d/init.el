72980a5d292eb3ada477caab8c347539ab49c07f
Matt Wittmann <mcwitt@gmail.com>
2020-04-27 09:21:40 -0700

    Move misc elisp

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -50,15 +50,6 @@
 ;; Highlight end-of-line whitespace only in prog-mode
 (add-hook 'prog-mode-hook (lambda () (setq-local show-trailing-whitespace t)))
 
-(defun mcw:increment-number-at-point ()
-  "Increment integer at the cursor position."
-  (interactive)
-  (skip-chars-backward "0-9")
-  (or (looking-at "[0-9]+")
-      (error "No number at point"))
-  (replace-match (number-to-string (1+ (string-to-number (match-string 0))))))
-
-(global-set-key (kbd "C-c +") #'mcw:increment-number-at-point)
 
 ;;; Package settings
 
