e97c1dcce611fd30fd14dd1a3d55ffc6165900ed
Matt Wittmann <mcwitt@gmail.com>
2020-04-30 20:34:17 -0700

    Add parens to fix formatting

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -654,12 +654,12 @@
   (after-init . org-roam-mode)
   :custom
   (org-roam-directory mcw:org-notes-notes-directory)
-  :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert)))
+  :bind ((:map org-roam-mode-map
+               (("C-c n l" . org-roam)
+                ("C-c n f" . org-roam-find-file)
+                ("C-c n g" . org-roam-graph)))
+         (:map org-mode-map
+               (("C-c n i" . org-roam-insert))))
   :config
   (require 'org-roam-protocol))
 
