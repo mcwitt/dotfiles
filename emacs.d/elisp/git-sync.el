d74be9fda289c69f5481773eae845dcdd7506916
Matt Wittmann <mcwitt@gmail.com>
2020-06-18 09:07:59 -0700

    Switch to use git-annex

--- emacs.d/elisp/git-sync.el
+++ emacs.d/elisp/git-sync.el
@@ -2,25 +2,27 @@
 ;;; Commentary:
 ;;; Code:
 
-(defvar git-sync-buffer-name "*async git-sync*")
-(defvar git-sync-command "git-sync")
+(defvar git-sync-command "git-annex sync")
+(defvar git-sync-buffer-name (concat "*async " git-sync-command "*"))
 
 (defun git-sync-sentinel (process event)
   "Watches the git-sync PROCESS for an EVENT indicating a successful sync and closes the window."
   (message event)
   (cond ((string-match-p "finished" event)
-         (message "git-sync successful")
+         (message (concat git-sync-command " successful"))
          (kill-buffer git-sync-buffer-name))
         ((string-match-p "\\(exited\\|dumped\\)" event)
-         (message "git-sync failed")
-         (when (yes-or-no-p "Error running git-sync. Switch to output?")
+         (message (concat git-sync-command " failed"))
+         (when
+             (yes-or-no-p
+              (concat "Error running '" git-sync-command "'. Switch to output?"))
            (switch-to-buffer git-sync-buffer-name)))))
 
 (defun git-sync ()
   "Run git-sync as an async process."
   (interactive)
   (let* ((process (start-process-shell-command
-                   "git-sync"
+                   git-sync-command
                    git-sync-buffer-name
                    git-sync-command)))
     (set-process-sentinel process 'git-sync-sentinel)))
