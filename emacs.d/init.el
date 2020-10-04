4c19dcbdda6733df2bc802ba1a8806134b3c52a7
Matt Wittmann <mcwitt@gmail.com>
2020-10-04 10:47:10 -0700

    Use unwrapped haskell-language-server

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -369,11 +369,7 @@
   :hook (haskell-mode . flycheck-haskell-setup))
 
 (use-package lsp-haskell
-  :config
-  (setq lsp-haskell-process-path-hie "haskell-language-server-wrapper")
-  ;; Comment/uncomment this line to see interactions between lsp client/server.
-  ;;(setq lsp-log-io t)
-  )
+  :init (setq lsp-haskell-server-path "haskell-language-server"))
 
 ;; Haskell code formatter
 (use-package ormolu
